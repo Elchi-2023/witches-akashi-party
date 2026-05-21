ce444b1cd9bf01d5ce33b5f09f394ce52b445d3d
SyntaxNyah <235356625+SyntaxNyah@users.noreply.github.com>
2026-05-21 17:03:40 +0100

    ok test done now the upstream
    test done http 2 has qt bugs

--- src/serverpublisher.cpp
+++ src/serverpublisher.cpp
@@ -53,7 +53,6 @@ void ServerPublisher::publishServer()
     if (serverlist.isValid()) {
         QNetworkRequest request(serverlist);
         request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-        request.setAttribute(QNetworkRequest::Http2AllowedAttribute, false);
 
         QJsonObject serverinfo;
         if (!ConfigManager::serverDomainName().trimmed().isEmpty()) {
@@ -68,9 +67,7 @@ void ServerPublisher::publishServer()
         serverinfo["name"] = ConfigManager::serverName();
         serverinfo["description"] = ConfigManager::serverDescription();
 
-        const QByteArray body = QJsonDocument(serverinfo).toJson(QJsonDocument::Compact);
-        qInfo().noquote() << "Advertising to masterserver. URL:" << serverlist.toString() << "POST body:" << QString::fromUtf8(body);
-        m_manager->post(request, body);
+        m_manager->post(request, QJsonDocument(serverinfo).toJson());
     }
     else {
         qWarning() << "Failed to advertise server. Serverlist URL is not valid. URL:" << serverlist.toString();
