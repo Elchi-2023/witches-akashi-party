aeed484e47c80025cc0b5c0b24ae660dc9fb0d22
SyntaxNyah <235356625+SyntaxNyah@users.noreply.github.com>
2026-05-21 09:50:39 +0100

    Refactor server advertisement logging and request body
    Uh more MS logging ughhhhhhhhhhhhhhhhh I HATE AO

--- src/serverpublisher.cpp
+++ src/serverpublisher.cpp
@@ -67,7 +67,9 @@ void ServerPublisher::publishServer()
         serverinfo["name"] = ConfigManager::serverName();
         serverinfo["description"] = ConfigManager::serverDescription();
 
-        m_manager->post(request, QJsonDocument(serverinfo).toJson());
+        const QByteArray body = QJsonDocument(serverinfo).toJson(QJsonDocument::Compact);
+        qInfo().noquote() << "Advertising to masterserver. URL:" << serverlist.toString() << "POST body:" << QString::fromUtf8(body);
+        m_manager->post(request, body);
     }
     else {
         qWarning() << "Failed to advertise server. Serverlist URL is not valid. URL:" << serverlist.toString();
@@ -77,6 +79,7 @@ void ServerPublisher::publishServer()
 void ServerPublisher::finished(QNetworkReply *f_reply)
 {
     QNetworkReply *reply(f_reply);
+    reply->deleteLater();
     QString remote_url = reply->url().toString();
 
     if (reply->error() != QNetworkReply::NoError) {
