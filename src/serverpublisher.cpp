7c2a118573166b2467cf73d10ddd990e6e790db4
SyntaxNyah <235356625+SyntaxNyah@users.noreply.github.com>
2026-05-21 12:49:04 +0100

    removed logging fuck ms3 pmo
    reverted i aint debugging that shit no more

--- src/serverpublisher.cpp
+++ src/serverpublisher.cpp
@@ -67,9 +67,7 @@ void ServerPublisher::publishServer()
         serverinfo["name"] = ConfigManager::serverName();
         serverinfo["description"] = ConfigManager::serverDescription();
 
-        const QByteArray body = QJsonDocument(serverinfo).toJson(QJsonDocument::Compact);
-        qInfo().noquote() << "Advertising to masterserver. URL:" << serverlist.toString() << "POST body:" << QString::fromUtf8(body);
-        m_manager->post(request, body);
+        m_manager->post(request, QJsonDocument(serverinfo).toJson());
     }
     else {
         qWarning() << "Failed to advertise server. Serverlist URL is not valid. URL:" << serverlist.toString();
