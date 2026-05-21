893fb1ae757e3a5565de9f9cccc71e9eb615c057
SyntaxNyah <235356625+SyntaxNyah@users.noreply.github.com>
2026-05-21 09:55:46 +0100

    YAY MORE MS LOGGING I FUCKING LOVE MS 3
    MS 3 LOGGING WAHOO FML. JSON PAYLOAD

--- src/serverpublisher.cpp
+++ src/serverpublisher.cpp
@@ -67,7 +67,9 @@ void ServerPublisher::publishServer()
         serverinfo["name"] = ConfigManager::serverName();
         serverinfo["description"] = ConfigManager::serverDescription();
 
-        m_manager->post(request, QJsonDocument(serverinfo).toJson());
+        const QByteArray body = QJsonDocument(serverinfo).toJson(QJsonDocument::Compact);
+        qInfo().noquote() << "Advertising to masterserver. URL:" << serverlist.toString() << "POST body:" << QString::fromUtf8(body);
+        m_manager->post(request, body);
     }
     else {
         qWarning() << "Failed to advertise server. Serverlist URL is not valid. URL:" << serverlist.toString();
