e5eabb58b8bb3f43ca8ead87d44fcb88aae9ea0c
Ganty <48977410+Ganty1999@users.noreply.github.com>
2026-06-28 20:11:51 +0700

    Update db_manager.cpp

--- src/db_manager.cpp
+++ src/db_manager.cpp
@@ -190,7 +190,7 @@ bool DBManager::invalidateBan(int id)
     ban_exists.addBindValue(id);
     ban_exists.exec();
 
-    if (!ban_exists.first()|| ban_exists.value(0).toInt() < 1)
+    if (!ban_exists.first())
         return false;
 
     QSqlQuery query;
